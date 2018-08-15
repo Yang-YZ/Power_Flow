47d9158ac9ca0f3081b031a69b94bd4f183817b1
Yang-YZ <31905385+Yang-YZ@users.noreply.github.com>
2018-08-15 00:34:14 -0700

    Add cs codeset with a UI and sample data.

---
+++ PowerFlow/CommonFunctions.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PowerFlow
+{
+    public static class CommonFunctions
+    {
+        public static void FindMaxAbs(List<double> data, out double max)
+        {
+            //  find the maximum absolute value from list data and return the maximum value.
+
+            try
+            {
+                max = Math.Abs(data[0]);
+                foreach (double value in data)
+                {
+                    if (Math.Abs(value) > max)
+                    {
+                        max = Math.Abs(value);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                max = 0.0;
+                MessageBox.Show("Error in Finding Maximum: " + ex.Message);
+            }
+        }
+
+        public static void Ybus(List<List<string>> bus, List<List<string>> branch, out double[,] Gbus, out double[,] Bbus)
+        {
+            // return Gbus, Bbus (i.e. Ybus) matrix from bus data and branch data.
+
+            #region Useful infomation from bus and branch
+
+            List<int> BusNO = bus[0].ToList().Select(int.Parse).ToList();
+            List<int> BusType = bus[6].ToList().Select(int.Parse).ToList();
+            List<double> VolMag = bus[7].ToList().Select(double.Parse).ToList();
+            List<double> VolAng = bus[8].ToList().Select(double.Parse).ToList();
+            List<double> LoadMW = bus[9].ToList().Select(double.Parse).ToList();
+            List<double> LoadMVAR = bus[10].ToList().Select(double.Parse).ToList();
+            List<double> GenMW = bus[11].ToList().Select(double.Parse).ToList();
+            List<double> GenMVAR = bus[12].ToList().Select(double.Parse).ToList();
+            List<double> busG = bus[17].ToList().Select(double.Parse).ToList();
+            List<double> busB = bus[18].ToList().Select(double.Parse).ToList();
+
+            List<int> fromBus = branch[0].ToList().Select(int.Parse).ToList();
+            List<int> toBus = branch[1].ToList().Select(int.Parse).ToList();
+            List<double> LineR = branch[6].ToList().Select(double.Parse).ToList();
+            List<double> LineX = branch[7].ToList().Select(double.Parse).ToList();
+            List<double> LineB = branch[8].ToList().Select(double.Parse).ToList();
+
+            #endregion
+
+            // initialize.
+            Gbus = new double[BusNO.Count, BusNO.Count];
+            Bbus = new double[BusNO.Count, BusNO.Count];
+
+            //  generate Gbus and Bbus.
+            try
+            {
+                for (int i = 0; i < fromBus.Count(); ++i)
+                {
+                    int from = fromBus[i] - 1;
+                    int to = toBus[i] - 1;
+                    Gbus[from, from] = Gbus[from, from] + LineR[i] / (LineR[i] * LineR[i] + LineX[i] * LineX[i]);
+                    Bbus[from, from] = Bbus[from, from] - LineX[i] / (LineR[i] * LineR[i] + LineX[i] * LineX[i]) + 0.5 * LineB[i];
+
+                    Gbus[to, to] = Gbus[to, to] + LineR[i] / (LineR[i] * LineR[i] + LineX[i] * LineX[i]);
+                    Bbus[to, to] = Bbus[to, to] - LineX[i] / (LineR[i] * LineR[i] + LineX[i] * LineX[i]) + 0.5 * LineB[i];
+
+                    Gbus[from, to] = Gbus[from, to] - LineR[i] / (LineR[i] * LineR[i] + LineX[i] * LineX[i]);
+                    Bbus[from, to] = Bbus[from, to] + LineX[i] / (LineR[i] * LineR[i] + LineX[i] * LineX[i]);
+
+                    Gbus[to, from] = Gbus[to, from] - LineR[i] / (LineR[i] * LineR[i] + LineX[i] * LineX[i]);
+                    Bbus[to, from] = Bbus[to, from] + LineX[i] / (LineR[i] * LineR[i] + LineX[i] * LineX[i]);
+                }                
+
+                for(int j = 0; j < BusNO.Count; ++j)
+                {
+                    if(busG[j] != 0)
+                    {
+                        Gbus[j, j] = Gbus[j, j] + busG[j];
+                    }
+                    if(busB[j] != 0)
+                    {
+                        Bbus[j, j] = Bbus[j, j] + busB[j];
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error in forming Ybus: " + ex.Message);
+            }
+        }
+
+        public static void Jacobian(List<int> BusNO, List<int> BusType, List<double> VolAng, List<double> VolMag, List<double> Pi, List<double> Qi, double[,] Gbus, double[,] Bbus, out double[,] Jacobian, out List<double> dPQ)
+        {
+            // return the Jacobian matrix from Gbus and Bbus matrix.
+
+            // find the bus type index.
+            List<int> PQ, PV, PVPQ, slack;
+            FindBusType(BusNO, BusType, out PQ, out PV, out PVPQ, out slack);
+
+            // initialize.
+            Jacobian = new double[PVPQ.Count + PQ.Count, PVPQ.Count + PQ.Count];
+            List<double> dP = new List<double>();
+            List<double> dQ = new List<double>();
+            dPQ = new List<double>();
+            double[,] J11 = new double[PVPQ.Count, PVPQ.Count];
+            double[,] J12 = new double[PVPQ.Count, PQ.Count];
+            double[,] J21 = new double[PQ.Count, PVPQ.Count];
+            double[,] J22 = new double[PQ.Count, PQ.Count];
+
+            // deltaP & Jacobian J11 i/i
+            foreach (int i in PVPQ)
+            {
+                double sigema = 0.0, sigemaJ = 0.0;
+                for(int j = 0; j < BusNO.Count; ++j)
+                {
+                    sigema = sigema + VolMag[j]*(Gbus[i,j]*(Math.Cos(VolAng[i]-VolAng[j])) + Bbus[i,j]*(Math.Sin(VolAng[i]-VolAng[j])));
+                    sigemaJ = sigemaJ + VolMag[j]*(Gbus[i,j]*((-1)*Math.Sin(VolAng[i]-VolAng[j])) + Bbus[i,j]*(Math.Cos(VolAng[i]-VolAng[j])));
+                }
+                dP.Add(Pi[i] - VolMag[i]*sigema);
+                dPQ.Add(Pi[i] - VolMag[i] * sigema);
+                J11[i - 1, i - 1] = VolMag[i] * sigemaJ - VolMag[i] * VolMag[i] * Bbus[i, i];
+                Jacobian[i - 1, i - 1] = J11[i - 1, i - 1];
+            }
+
+            // Jacobian J11 i/j
+            foreach (int i in PVPQ)
+            {
+                foreach (int j in PVPQ)
+                {
+                    if (i != j)
+                    {
+                        int indexi = PVPQ.FindIndex(x => x==i);
+                        int indexj = PVPQ.FindIndex(x => x==j);
+                        J11[indexi, indexj] = VolMag[i]*VolMag[j]*(Gbus[i,j]*(Math.Sin(VolAng[i]-VolAng[j]))-Bbus[i,j]*(Math.Cos(VolAng[i]-VolAng[j])));
+                        Jacobian[indexi, indexj] = J11[indexi, indexj];
+                    }
+                }
+            }
+
+            // Jacobian J12 i/i  &  J21 i/i
+            foreach (int i in PQ)
+            {
+                double sigemaJ = 0.0;
+                for(int j = 0; j < BusNO.Count; ++j)
+                {
+                    sigemaJ = sigemaJ + VolMag[j]*(Gbus[i,j]*(Math.Cos(VolAng[i]-VolAng[j])) + Bbus[i,j]*(Math.Sin(VolAng[i]-VolAng[j])));
+                }
+                int indexi = PVPQ.FindIndex(x => x==i);
+                int indexj = PQ.FindIndex(x => x==i);
+                J12[indexi, indexj] = sigemaJ + VolMag[i] * Gbus[i, i];
+                J21[indexj, indexi] = VolMag[i] * sigemaJ - (VolMag[i] * VolMag[i]) * Gbus[i, i];
+                Jacobian[indexi, indexj + PVPQ.Count] = J12[indexi, indexj];
+                Jacobian[indexj + PVPQ.Count, indexi] = J21[indexj, indexi];
+            }
+
+            // Jacobian J12 i/j
+            foreach (int i in PVPQ)
+            {
+                foreach (int j in PQ)
+                {
+                    if (i != j)
+                    {
+                        int indexi = PVPQ.FindIndex(x => x == i);
+                        int indexj = PQ.FindIndex(x => x == j);
+                        J12[indexi, indexj] = VolMag[i] * (Gbus[i, j] * (Math.Cos(VolAng[i] - VolAng[j])) + Bbus[i, j] * (Math.Sin(VolAng[i] - VolAng[j])));
+                        Jacobian[indexi, indexj + PVPQ.Count] = J12[indexi, indexj];
+                    }
+                }
+            }
+
+            // Jacobian J21 i/j
+            foreach (int i in PQ)
+            {
+                foreach (int j in PVPQ)
+                {
+                    if (i != j)
+                    {
+                        int indexi = PQ.FindIndex(x => x == i);
+                        int indexj = PVPQ.FindIndex(x => x == j);
+                        J21[indexi, indexj] = VolMag[i] * VolMag[j] * (-Gbus[i, j] * (Math.Cos(VolAng[i] - VolAng[j])) - Bbus[i, j] * (Math.Sin(VolAng[i] - VolAng[j])));
+                        Jacobian[indexi + PVPQ.Count, indexj] = J21[indexi, indexj];
+                    }
+                }
+            }
+
+            // deltaQ & Jacobian J22 i/i
+            foreach (int i in PQ)
+            {
+                double sigema = 0.0;
+                for (int j = 0; j < BusNO.Count; ++j)
+                {
+                    sigema = sigema + VolMag[j] * (Gbus[i, j] * (Math.Sin(VolAng[i] - VolAng[j])) - Bbus[i, j] * (Math.Cos(VolAng[i] - VolAng[j])));
+                }
+                dQ.Add(Qi[i] - VolMag[i] * sigema); 
+                dPQ.Add(Qi[i] - VolMag[i] * sigema);
+                int indexi = PQ.FindIndex(x => x == i);
+                J22[indexi, indexi] = sigema - VolMag[i] * Bbus[i, i];
+                Jacobian[indexi + PVPQ.Count, indexi + PVPQ.Count] = J22[indexi, indexi];
+            }
+
+            // Jacobian J22 i/j
+            foreach (int i in PQ)
+            {
+                foreach (int j in PQ)
+                {
+                    if (i != j)
+                    {
+                        int indexi = PQ.FindIndex(x => x == i);
+                        int indexj = PQ.FindIndex(x => x == j);
+                        J22[indexi, indexj] = VolMag[i] * (Gbus[i, j] * (Math.Sin(VolAng[i] - VolAng[j])) - Bbus[i, j] * (Math.Cos(VolAng[i] - VolAng[j])));
+                        Jacobian[indexi + PVPQ.Count, indexj + PVPQ.Count] = J22[indexi, indexj];
+                    }
+                }
+            }
+        }
+
+        public static void FindBusType(List<int> BusNO, List<int> BusType, out List<int> PQ, out List<int> PV, out List<int> PVPQ, out List<int> slack)
+        {
+            // find the index of PQ, PV and slack buses.
+
+            PQ = new List<int>();
+            PV = new List<int>();
+            PVPQ = new List<int>();
+            slack = new List<int>();
+
+            for (int i = 0; i < BusNO.Count; ++i)
+            {
+                int type = BusType[i];
+                switch (type)
+                {
+                    case 0:
+                        PQ.Add(i);
+                        PVPQ.Add(i);
+                        break;
+                    case 1:
+                        PQ.Add(i);
+                        PVPQ.Add(i);
+                        break;
+                    case 2:
+                        PV.Add(i);
+                        PVPQ.Add(i);
+                        break;
+                    case 3:
+                        slack.Add(i);
+                        break;
+                    default:
+                        MessageBox.Show("Error in finding bus type.");
+                        break;
+                }
+            }
+        }
+
+        public static void LUFact(double[,] A, List<double> b, out List<double> x)
+        {
+            // use LU factorization to get the inverse of a matrix.
+
+            int row = A.GetLength(0);
+
+            //  get Q matrix using Crout's algorithm
+            double[,] Q;
+            crout(A, out Q);
+
+            //  forward substitution to get y
+            List<double> y = new List<double>();
+            for (int i = 0; i < row; ++i)
+            {
+                y.Add(0.0);
+            }
+            for (int k = 0; k < row; ++k)
+            {
+                double sigmay = 0.0;
+                for (int j = 0; j < k; ++j)
+                {
+                    sigmay = sigmay + Q[k, j] * y[j];
+                }
+                y[k] = (1 / Q[k, k]) * (b[k] - sigmay);
+            }
+
+            //  backward substitution to get x            
+            x = new List<double>();
+            for (int i = 0; i < row; ++i)
+            {
+                x.Add(0.0);
+            }
+            for (int k = row-1; k >= 0; --k)
+            {
+                double sigmax = 0.0;
+                for (int j = (k+1); j < row; ++j)
+                {
+                    sigmax = sigmax + Q[k,j]*x[j];
+                }
+                x[k] = y[k] - sigmax;
+            }
+        }
+
+        public static void crout(double[,] A, out double[,] Q)
+        {
+            //  use Crout's algorithm to compute the element of Q.
+
+            int row = A.GetLength(0);
+            int col = A.GetLength(1);
+            Q = new double[row, col];
+
+            for (int j = 0; j < col; ++j)
+            {
+                // the jth column
+                for (int k = j; k < row; ++k)
+                {
+                    double sigmacol = 0;
+                    for (int i = 0; i < j; ++i)
+                    {
+                        sigmacol = sigmacol + Q[k,i]*Q[i,j];
+                    }
+                    Q[k,j] = A[k,j] - sigmacol;
+                }
+                // the jth row
+                if (Q[j,j] != 0)
+                {
+                    for (int k = (j+1); k < col; ++k)
+                    {
+                        double sigmarow = 0;
+                        for (int i = 0; i < j; ++i)
+                        {
+                            sigmarow = sigmarow + Q[j,i]*Q[i,k];
+                        }
+                        Q[j,k] = (1/Q[j,j])*(A[j,k]-sigmarow);
+                    }
+                }
+            }
+        }            
+            
+    }
+}
---
+++ PowerFlow/PowerFlow.cs
@@ -0,0 +1,290 @@
+﻿//////////////////////////////////////////////////////////
+// Copyright (c) 2016 All Rights Reserved
+// Author:          Yang Zheng 
+// Last Update:     10/18/2016
+// Summary:         Power flow using Newton Raphson method
+// Contact:         yang.zheng@wsu.edu
+//////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PowerFlow
+{
+    public partial class PowerFlow : Form
+    {
+        public List<List<string>> bus = new List<List<string>>();
+        public List<List<string>> branch = new List<List<string>>();
+        public static double stoppingError = 0.01;
+
+        public PowerFlow()
+        {
+            InitializeComponent();
+        }
+
+        private void readBus_Click(object sender, EventArgs e)
+        {
+            //  read bus file and generate a string list of list "bus".
+
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "CSV Files (*.csv)|*.csv";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string[] lines = File.ReadAllLines(dlg.FileName);
+                    var sep = new[] { "," };
+                    for (int i = 0; i < lines.Count(); i++)
+                    {
+                        string[] cols = lines[i].Split(sep, StringSplitOptions.RemoveEmptyEntries);
+                        bus.Add(new List<string>());
+                        for (int j = 0; j < cols.Count(); j++)
+                        {
+                            bus[i].Add(cols[j]);
+                        }
+                    }
+                    MessageBox.Show("Bus data imported.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Could not read bus data. Original Error: " + ex.Message);
+                }
+            }
+        }
+        
+        private void readBranch_Click(object sender, EventArgs e)
+        {
+            //  read branch file and generate a string list of list "branch".
+
+            OpenFileDialog dlg = new OpenFileDialog();
+            dlg.Filter = "CSV Files (*.csv)|*.csv";
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string[] lines = File.ReadAllLines(dlg.FileName);
+                    var sep = new[] { "," };
+                    for (int i = 0; i < lines.Count(); i++)
+                    {
+                        string[] cols = lines[i].Split(sep, StringSplitOptions.RemoveEmptyEntries);
+                        branch.Add(new List<string>());
+                        for (int j = 0; j < cols.Count(); j++)
+                        {
+                            branch[i].Add(cols[j]);
+                        }
+                    }
+                    MessageBox.Show("Branch data imported.");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Could not read branch data. Original Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void NewtonRaphson_Click(object sender, EventArgs e)
+        {
+            //  start Newton-Raphson power flow calculation when user clicks the button.
+
+            try
+            {
+                #region Useful infomation from bus and branch
+
+                List<int> BusNO = bus[0].ToList().Select(int.Parse).ToList();
+                List<int> BusType = bus[6].ToList().Select(int.Parse).ToList();
+                List<double> VolMag = bus[7].ToList().Select(double.Parse).ToList();
+                List<double> VolAng = bus[8].ToList().Select(double.Parse).ToList();
+                List<double> LoadMW = bus[9].ToList().Select(double.Parse).ToList();
+                List<double> LoadMVAR = bus[10].ToList().Select(double.Parse).ToList();
+                List<double> GenMW = bus[11].ToList().Select(double.Parse).ToList();
+                List<double> GenMVAR = bus[12].ToList().Select(double.Parse).ToList();
+                List<double> busG = bus[17].ToList().Select(double.Parse).ToList();
+                List<double> busB = bus[18].ToList().Select(double.Parse).ToList();
+
+                List<int> fromBus = branch[0].ToList().Select(int.Parse).ToList();
+                List<int> toBus = branch[1].ToList().Select(int.Parse).ToList();
+                List<double> LineR = branch[6].ToList().Select(double.Parse).ToList();
+                List<double> LineX = branch[7].ToList().Select(double.Parse).ToList();
+                List<double> LineB = branch[8].ToList().Select(double.Parse).ToList();
+
+                #endregion
+
+                //  generate the Gbus and Bbus matrix.
+                double[,] Gbus, Bbus;
+                CommonFunctions.Ybus(bus, branch, out Gbus, out Bbus);
+
+                //  find the PQ, PV, PVPQ, slack bus index.
+                List<int> PQ, PV, PVPQ, slack;
+                CommonFunctions.FindBusType(BusNO, BusType, out PQ, out PV, out PVPQ, out slack);
+
+                //  initialize Pi, Qi, VolMag, VolAng according to bus type.
+                List<double> Pi = new List<double>();
+                List<double> Qi = new List<double>();
+                for (int i = 0; i < BusNO.Count; ++i)
+                {
+                    if (BusType[i] == 3)
+                    {
+                        //  slack bus, set GenMW = 0, GenMVAR = 0;
+                        GenMW[i] = 0;
+                        GenMVAR[i] = 0;
+                    }
+                    if (BusType[i] == 2)
+                    {
+                        //  PV bus, set GenMVAR = 0, VolAng = 0;
+                        GenMVAR[i] = 0;
+                        VolAng[i] = 0;
+                    }
+                    if (BusType[i] == 1 || BusType[i] == 0)
+                    {
+                        //  PQ bus, set VolMag = 1, VolAng = 0;
+                        VolMag[i] = 1;
+                        VolAng[i] = 0;
+                    }
+                    //  Pinjection = Pgen - Pload, must convert to p.u.!
+                    Pi.Add((GenMW[i] - LoadMW[i]) / 100);
+                    Qi.Add((GenMVAR[i] - LoadMVAR[i]) / 100);
+                }
+
+                //  initialize the iteration count and error.
+                int iter = 0;
+                double error = 1.0;
+
+                //  Newton Raphson iteration.
+                while (error > stoppingError)
+                {
+                    // generate Jacobian matrix and mismatch deltaP and deltaQ.
+                    double[,] Jacobian;
+                    List<double> dPQ;
+                    CommonFunctions.Jacobian(BusNO, BusType, VolAng, VolMag, Pi, Qi, Gbus, Bbus, out Jacobian, out dPQ);
+
+                    // compute [dVol] by inverse of Jacobian multiply [dPQ].
+                    List<double> dVol;
+                    CommonFunctions.LUFact(Jacobian, dPQ, out dVol);
+
+                    // update the VolAng and VolMag, 
+                    // start another iteration until stopping error is satisfied.
+                    foreach (int i in PVPQ)
+                    {
+                        int index = PVPQ.FindIndex(x => x == i);
+                        VolAng[i] = VolAng[i] + dVol[index];
+                    }
+                    foreach (int i in PQ)
+                    {
+                        int index = PQ.FindIndex(x => x == i) + PVPQ.Count;
+                        VolMag[i] = VolMag[i] + dVol[index];
+                    }
+
+                    iter++;
+
+                    // update the error.
+                    CommonFunctions.FindMaxAbs(dPQ, out error);
+                }
+
+                //  display the power flow results.
+                UpdateResults(VolAng, VolMag, iter);
+
+                //  results save to a txt file.
+                SaveResults(VolAng, VolMag);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Please check data format! " + ex.Message);
+            }
+        }
+
+        private void UpdateResults(List<double> VolAng, List<double> VolMag, int iter)
+        {
+            //  display the results in the main window.
+            //  only display the first 10 voltage magnitudes and angles.
+
+            textBuses.Text = (bus[0].Count).ToString();
+            textIter.Text = (iter).ToString();
+            textBox1.Text = (VolAng[0]).ToString("0.00");
+            textBox2.Text = (VolAng[1]).ToString("0.00");
+            textBox3.Text = (VolAng[2]).ToString("0.00");
+            textBox4.Text = (VolAng[3]).ToString("0.00");
+            textBox5.Text = (VolAng[4]).ToString("0.00");
+            textBox6.Text = (VolAng[5]).ToString("0.00");
+            textBox7.Text = (VolAng[6]).ToString("0.00");
+            textBox8.Text = (VolAng[7]).ToString("0.00");
+            textBox9.Text = (VolAng[8]).ToString("0.00");
+            textBox10.Text = (VolAng[9]).ToString("0.00");
+            textBox11.Text = (VolMag[0]).ToString("0.00");
+            textBox12.Text = (VolMag[1]).ToString("0.00");
+            textBox13.Text = (VolMag[2]).ToString("0.00");
+            textBox14.Text = (VolMag[3]).ToString("0.00");
+            textBox15.Text = (VolMag[4]).ToString("0.00");
+            textBox16.Text = (VolMag[5]).ToString("0.00");
+            textBox17.Text = (VolMag[6]).ToString("0.00");
+            textBox18.Text = (VolMag[7]).ToString("0.00");
+            textBox19.Text = (VolMag[8]).ToString("0.00");
+            textBox20.Text = (VolMag[9]).ToString("0.00");
+        }
+
+        private void SaveResults(List<double> VolAng, List<double> VolMag)
+        {
+            //  save the full results to a txt file named "results.stg".
+
+            StreamWriter settingfile = new StreamWriter(".\\results.stg");
+            settingfile.WriteLine("Voltage Angle:");
+            for (int i = 0; i < VolAng.Count; ++i)
+            {
+                settingfile.WriteLine(VolAng[i]);
+            }
+            settingfile.WriteLine("Voltage Magnitude:");
+            for (int i = 0; i < VolMag.Count; ++i)
+            {
+                settingfile.WriteLine(VolMag[i]);
+            }
+            settingfile.Close();
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            //  clear bus data and branch data when user clicks the button.
+
+            bus = new List<List<string>>();
+            branch = new List<List<string>>();
+            MessageBox.Show("Data is cleared.");
+        }
+
+        private void updateSetting_Click(object sender, EventArgs e)
+        {
+            //  set the stopping error according to user's input.
+
+            StreamWriter settingfile = new StreamWriter(".\\settings.stg");
+            try
+            {
+                settingfile.WriteLine(double.Parse(errorSetting.Text));
+                stoppingError = double.Parse(errorSetting.Text);
+            }
+            catch
+            {
+                settingfile.WriteLine("0.01");
+                stoppingError = 0.01;
+            }
+            settingfile.Close();
+        }
+
+        private void defaultSetting_Click(object sender, EventArgs e)
+        {
+            //  the default value for stopping error is 0.01.
+
+            errorSetting.Text = "0.01";
+        }
+
+        private void FastDecoupled_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This part is under construction. Thank you for your patience.");
+        }
+        
+    }
+}
